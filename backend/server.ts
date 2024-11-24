257cc74a36621a45c7e65c7528846e51fc12b642
Istiak <mhistiak2@gmail.com>
2024-11-24 17:39:03 +0600

    Start Server -backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,2 +1,12 @@
+// Imports
+import app from "./src/app";
 
-console.log('Hello from Node.js!');
+
+// Start the server
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+startServer();
---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+/***
+ * Title: BookBreeze API
+ * Author: Istiak Ahammad
+ * Date: 2022-06-14
+ * Description: BookBreeze API, API for book store
+***/
+
+// Imports
+import express from 'express';
+
+// Constants
+const app = express();
+
+
+
+
+// export
+export default app;
