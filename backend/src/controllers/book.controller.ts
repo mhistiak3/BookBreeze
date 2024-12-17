bd3477f69e442ed4a4cff7da107c45dc675f17cd
Istiak <mhistiak2@gmail.com>
2024-12-17 21:38:37 +0600

    create book

--- backend/src/controllers/book.controller.ts
+++ backend/src/controllers/book.controller.ts
@@ -2,9 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import cloudinary from "../config/cloudinary";
 import { v4 as uuidv4 } from "uuid";
-
+import BookModel from "../models/book.model";
+import { UploadApiResponse, UploadApiErrorResponse } from "cloudinary";
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { title, genre } = req.body;
+    // validation
+    if (!title  || !genre) {
+      throw createHttpError(400, "All fields are required");
+    }
     // File validation
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
     if (!files.coverImage || !files.file) {
@@ -26,28 +32,44 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       public_id: coverImagePublicId,
       folder: "bookbreeze",
     });
-    console.log("Cover Image Uploaded:", uploadCoverImage);
-
     // Upload PDF file to Cloudinary using upload_stream
   const filePublicId = `${uuidv4()}.pdf`;
-    const uploadFile = await new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream(
-        {
-          resource_type: "raw",
-          public_id: `file/${filePublicId}`,
-          folder: "bookbreeze",
-        },
-        (error, result) => {
-          if (error) {
-            return reject(error);
+    const uploadFile = await new Promise<UploadApiResponse>(
+      (resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          {
+            resource_type: "raw",
+            public_id: `bookbreeze/${filePublicId}`,
+            folder: "bookbreeze",
+          },
+          (
+            error: UploadApiErrorResponse | undefined,
+            result: UploadApiResponse | undefined
+          ) => {
+            if (error) {
+              return reject(error); // Reject if there's an error
+            }
+            if (result) {
+              resolve(result); // Resolve with the result
+            }
           }
-          resolve(result);
-        }
-      );
-      stream.end(bookFile.buffer); 
+        );
+        stream.end(bookFile.buffer); // Send the file buffer
+      }
+    );
+
+    // create book
+    const book = await BookModel.create({
+      title,
+      author: "6745c24d502de7f3c8e305b5",
+      genre,
+      coverImage: uploadCoverImage.secure_url,
+      file: uploadFile.secure_url,
     });
 
-    console.log("PDF File Uploaded:", uploadFile);
+    if (!book) {
+      throw createHttpError(500, "Something went wrong during book creation");    
+    }
 
     // Response
     res.status(201).json({
@@ -55,7 +77,12 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     });
   } catch (error) {
     console.error("Error:", error);
-    next(createHttpError(500, "Something went wrong during file upload"));
+      let errorMessage = "Something went wrong during book creation";
+      // Check if error is an instance of Error
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+    next(createHttpError(500, errorMessage));
   }
 };
 
